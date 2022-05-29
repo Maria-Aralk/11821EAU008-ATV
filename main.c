21291e2a1007081b0152a4ecef10bd39c651759b
MARIA CLARA RODRIGUES <95433134+Maria-Aralk@users.noreply.github.com>
2022-05-29 15:19:23 -0300

    lab02

--- main.c
+++ main.c
@@ -1,323 +1,71 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-#include "app.h"
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-MC_HandleTypeDef hadc1;
-
-TM_HandleTypeDef htm;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_MC_Init(void);
-static void MX_TM_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
+#include <stdlib.h>
+int main(intargc,char*argv[])
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_MC_Init();
-  MX_TM_Init();
-  /* USER CODE BEGIN 2 */
-  app_init();
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+  while(1){};
+  /* Nao deveria chegar aqui */
+  return EXIT_SUCCESS;
   }
-  /* USER CODE END 3 */
-}
+  #define SRAM_START  0x20000000U/* Inicio da SRAM CORTEX-M */
+  #define SRAM_SIZE   (128U * 1024U)/* Tam. SRAM STM32F411 128K */
+  #define SRAM_END    ((SRAM_START) +
+                      (SRAM_SIZE))/* Final da SRAM STM32F411 */
+  #define STACK_START SRAM_END/* Inicio da Stack */
+  
+void reset_handler     (void);
+void nmi_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void hardfault_handler (void) __attribute__ ((weak, alias("default_handler")));
+void memmanage_handler (void) __attribute__ ((weak, alias("default_handler")));
+void busfault_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void usagefault_handler(void) __attribute__ ((weak, alias("default_handler")));
+void svc_handler       (void) __attribute__ ((weak, alias("default_handler")));
+void debugmon_handler  (void) __attribute__ ((weak, alias("default_handler")));
+void pendsv_handler    (void) __attribute__ ((weak, alias("default_handler")));
+void systick_handler   (void) __attribute__ ((weak, alias("default_handler")));
+externuint32_t_sdata;/* Inicio da secao .data */
+externuint32_t_edata;/* Fim da secao .data */
+externuint32_t_la_data;/* Endereco de carga na RAM da secao .data */
+externuint32_t_sbss;/* Inicio da secao .bss */
+externuint32_t_ebss;/* Fim da secao .bss */
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+uint32_tvectors[] __attribute__((section(".isr_vectors"))) =
+{STACK_START,/* 0x0000 0000 */
+(uint32_t)reset_handler,/* 0x0000 0004 */
+(uint32_t)nmi_handler,/* 0x0000 0008 */
+(uint32_t)hardfault_handler,/* 0x0000 000c */
+(uint32_t)memmanage_handler,/* 0x0000 0010 */
+(uint32_t)busfault_handler,/* 0x0000 0014 */
+(uint32_t)usagefault_handler,/* 0x0000 0018 */18
+0,/* 0x0000 001c */
+0,/* 0x0000 0020 */
+0,/* 0x0000 0024 */
+0,/* 0x0000 0028 */
+(uint32_t)svc_handler,/* 0x0000 002c */
+(uint32_t)debugmon_handler,/* 0x0000 0030 */
+0,/* 0x0000 0034 */
+(uint32_t)pendsv_handler,/* 0x0000 0038 */
+(uint32_t)systick_handler,/* 0x0000 003c */
+};
+void reset_handler(void){}
+void default_handler(void){while(1){};}
+void reset_handler(){
+  uint32_t i;
+/* Copia a secao .data para a RAM */
+uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+uint8_t* pDst =(uint8_t*)&_sdata;/* SRAM */
+uint8_t* pSrc =(uint8_t*)&_etext;/* FLASH */
+
+for(i =0; i < size; i++)
+{ 
+  *pDst++ = *pSrc++;
+  }/* Preenche a secao .bss com zero */
+size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+pDst = (uint8_t*)&_sbss;
+for(i =0; i < size; i++){
+*pDst++ =0;
+}
+/* Chama a funcao main() */
+main();}
+void default_handler(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
+  while(1){};
   }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-  RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief MC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_MC_Init(void)
-{
-
-  /* USER CODE BEGIN MC_Init 0 */
-
-  /* USER CODE END MC_Init 0 */
-
-  MC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN MC_Init 1 */
-
-  /* USER CODE END MC_Init 1 */
-  /** Configure the global features of the MC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc1.Instance = MC;
-  hadc1.Init.ClockPrescaler = MC_CLOCK_SYNC_PCLK_DIV4;
-  hadc1.Init.Resolution = MC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ENABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = MC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = MC_SOFTWARE_START;
-  hadc1.Init.DataAlign = MC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 2;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = MC_EOC_SINGLE_CONV;
-  if (HAL_MC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = MC_CHANNEL_3;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = MC_SAMPLETIME_28CYCLES;
-  if (HAL_MC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = MC_CHANNEL_4;
-  sConfig.Rank = 2;
-  sConfig.SamplingTime = MC_SAMPLETIME_56CYCLES;
-  if (HAL_MC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN MC_Init 2 */
-
-  /* USER CODE END MC_Init 2 */
-
-}
-
-/**
-  * @brief TM Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TM_Init 0 */
-
-  /* USER CODE END TM_Init 0 */
-
-  TM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TM_Init 1 */
-
-  /* USER CODE END TM_Init 1 */
-  htm.Instance = TM;
-  htm.Init.Prescaler = 0;
-  htm.Init.CounterMode = TM_COUNTERMODE_UP;
-  htm.Init.Period = 65535;
-  htm.Init.ClockDivision = TM_CLOCKDIVISION_DIV1;
-  htm.Init.AutoReloadPreload = TM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htm) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TM_ConfigClockSource(&htim, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TM_Init 2 */
-
-  /* USER CODE END TM_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : USER_LED_Pin */
-  GPIO_InitStruct.Pin = USER_LED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(USER_LED_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : USER_SW_Pin */
-  GPIO_InitStruct.Pin = USER_SW_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(USER_SW_GPIO_Port, &GPIO_InitStruct);
-
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
-
